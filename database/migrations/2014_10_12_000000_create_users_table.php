08f280fb802ff6567eded84f96d6e902589d3be4
Sjors <sjorsottjes@gmail.com>
2018-05-07 12:25:00 +0200

    set default timezone

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,7 +11,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('email')->unique();
-            $table->string('timezone')->default('UTC');
+            $table->string('timezone')->default('Europe/Amsterdam');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
