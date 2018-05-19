b3f8240b82570888c913257a942bfe6a40adc435
Sjors <sjorsottjes@gmail.com>
2018-05-19 13:42:39 +0200

    add a dd_delay helper

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -31,3 +31,25 @@ function interval(int $interval, $closure)
 
     $calls[$caller] = $callCount + 1;
 }
+
+/**
+ * Trigger a "dd()" after it has been called "timesCalled" times.
+ *
+ * @param $var
+ *
+ * @param int $timesCalled
+ */
+function dd_delay($var, int $timesCalled = 2)
+{
+    static $calls = [];
+
+    $caller = sha1(debug_backtrace()[0]['file'].'|'.debug_backtrace()[0]['line']);
+
+    $callCount = $calls[$caller] ?? 1;
+
+    if ($callCount === $timesCalled) {
+        dd($var);
+    }
+
+    $calls[$caller] = $callCount + 1;
+}
