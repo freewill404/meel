707e892bbe13c68819fd251b948d06745da81eae
Sjors <sjorsottjes@gmail.com>
2018-06-09 20:36:19 +0200

    add cleaner svg logo

---
+++ resources/views/helpers/svg/logo.blade.php
@@ -0,0 +1,43 @@
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid meet" viewBox="0 0 340 410">
+    <defs>
+        <path d="M335 18.68C335 95.94 335 314.06 335 391.32C335 398.87 328.87 405 321.32 405C258.06 405 81.94 405 18.68 405C11.13 405 5 398.87 5 391.32C5 314.06 5 95.94 5 18.68C5 11.13 11.13 5 18.68 5C81.94 5 258.06 5 321.32 5C328.87 5 335 11.13 335 18.68Z" id="bwt4dbKrX"></path>
+        <path d="M335 29L170.01 141L5 29" id="c42RQvOKap"></path>
+        <path d="M335 85L170.01 197L5 85" id="f4dy1TusRH"></path>
+        <path d="M335 141L170.01 253L5 141" id="c2AlRPowca"></path>
+        <path d="M170.01 405L170.01 253" id="b1CKNVE48m"></path>
+    </defs>
+    <g>
+        <g>
+            <g>
+                <use xlink:href="#bwt4dbKrX" opacity="1" fill="#000000" fill-opacity="0"></use>
+                <g>
+                    <use xlink:href="#bwt4dbKrX" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="10" stroke-opacity="1"></use>
+                </g>
+            </g>
+            <g>
+                <use xlink:href="#c42RQvOKap" opacity="1" fill="#000000" fill-opacity="0"></use>
+                <g>
+                    <use xlink:href="#c42RQvOKap" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="10" stroke-opacity="1"></use>
+                </g>
+            </g>
+            <g>
+                <use xlink:href="#f4dy1TusRH" opacity="1" fill="#000000" fill-opacity="0"></use>
+                <g>
+                    <use xlink:href="#f4dy1TusRH" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="10" stroke-opacity="1"></use>
+                </g>
+            </g>
+            <g>
+                <use xlink:href="#c2AlRPowca" opacity="1" fill="#000000" fill-opacity="0"></use>
+                <g>
+                    <use xlink:href="#c2AlRPowca" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="10" stroke-opacity="1"></use>
+                </g>
+            </g>
+            <g>
+                <use xlink:href="#b1CKNVE48m" opacity="1" fill="#000000" fill-opacity="0"></use>
+                <g>
+                    <use xlink:href="#b1CKNVE48m" opacity="1" fill-opacity="0" stroke="#000000" stroke-width="10" stroke-opacity="1"></use>
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
