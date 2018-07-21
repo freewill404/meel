06623cffe5e6dbc6a80c9f9e6ff4c9c7001f483a
Sjors <sjorsottjes@gmail.com>
2018-07-21 08:18:11 +0200

    vanity hash

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,7 +7,7 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
     return [
         'email'               => $faker->unique()->safeEmail,
         'timezone'            => 'Europe/Amsterdam',
-        'password'            => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password'            => '$2y$04$vmM7jNTINfByJhhXXLNdAuLnfE3TEXB9XmAlKwXhrB.mZmK/mEEL.', // secret
         'remember_token'      => str_random(10),
         'email_confirm_token' => null,
         'email_confirmed'     => true,
