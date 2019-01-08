b6fc9a1b3d1bbc156e63a0075c7f0fdb62d4142e
Sjors <sjorsottjes@gmail.com>
2019-01-08 07:55:38 +0100

    add margin

--- resources/views/schedules/ok.blade.php
+++ resources/views/schedules/ok.blade.php
@@ -1,10 +1,9 @@
 @extends('layout.base-template', [
-    'title' => 'Email schedule created! | Meel.me',
+    'title' => 'Meel.me',
 ])
 
 @section('content')
-
-    <div class="max-w-xs mx-auto mt-2 text-center">
+    <div class="max-w-xs mx-auto mt-8 text-center">
 
         <a class="text-black inline-block w-16 h-16" title="Meel.me" href="/">
             @include('helpers.svg.logo')
@@ -19,5 +18,4 @@
         <a href="/" class="btn block mx-8 mt-8">Back</a>
 
     </div>
-
 @endsection
